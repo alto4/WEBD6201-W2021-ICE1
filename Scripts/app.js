00796b7ed30631f1f7ace076606934ff9cacabf7
Scott Alton <scottaltonmusic@gmail.com>
2021-01-21 09:35:51 -0500

    Added contact form.

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,73 +5,53 @@
 // use strict forces variables to be declared as let, var, or const
 "use strict";
 
-(function ()
-{
-  function displayHome()
-  {
-    let paragraphOneText = 'This is a simple site to demonstrate DOM Manipulation for ICE 1';
-    let paragraphOneElement = document.getElementById('paragraphOne');
- 
+(function () {
+  function displayHome() {
+    let paragraphOneText =
+      "This is a simple site to demonstrate DOM Manipulation for ICE 1";
+    let paragraphOneElement = document.getElementById("paragraphOne");
+
     paragraphOneElement.textContent = paragraphOneText;
-    paragraphOneElement.className = 'fs-5 text-success';
-  
+    paragraphOneElement.className = "fs-5 text-success";
+
     // Create new element in memory
-    let newParagraph = document.createElement('p');
+    let newParagraph = document.createElement("p");
     // Configure element
-    newParagraph.setAttribute('id', 'paragraphTwo');
-    newParagraph.textContent = '...paragraph two injected into DOM!';
-    newParagraph.className = 'fs-5 text-danger'
+    newParagraph.setAttribute("id", "paragraphTwo");
+    newParagraph.textContent = "...paragraph two injected into DOM!";
+    newParagraph.className = "fs-5 text-danger";
     // Select the parent element
-    let heroContainer = document.querySelector('.hero');
-    heroContainer.className = 'my-5';
+    let heroContainer = document.querySelector(".hero");
     // Inject element into the DOM
     heroContainer.appendChild(newParagraph);
 
-    // Alternate way to inject element 
-    let paragraphDiv = document.createElement('div');
+    // Alternate way to inject element
+    let paragraphDiv = document.createElement("div");
     let paragraphThree = `<p id="paragraphThree" class="fs-7 fw-bold">This is the third paragraph, set in the middle using .before().</p>`;
     paragraphDiv.innerHTML = paragraphThree;
 
     // Insert new paragraph -> .before inserts right before node
     // newParagraph.before(paragraphDiv);
     newParagraph.after(paragraphDiv);
-   
+
     // Deletion
     // paragraphOneElement.remove();
     heroContainer.removeChild(paragraphOneElement);
-    
+
     // Update
     //heroContainer.firstElementChild.textContent = 'Hello there friends on the internet.';
-  
-    heroContainer.innerHTML = 
-    `<h1 id="firstHeading">Welcome to WEBD6201 - Lab 1</h1>
+
+    heroContainer.innerHTML = `<h1 id="firstHeading">Welcome to WEBD6201 - Lab 1</h1>
     <p id="paragraphOne" class="fs-3 fw-bold text-success">This is my first paragraph</p>
     `;
   }
-  function displayAbout()
-  {
-    
-  }
-  function displayProjects()
-  {
-    
-  }
-  function displayServices()
-  {
-    
-  }
-  function displayContact()
-  {
-    
-  }
+  function displayAbout() {}
+  function displayProjects() {}
+  function displayServices() {}
+  function displayContact() {}
 
-
-
-
-
-  function Start()
-  {
-    console.log('App started...');
+  function Start() {
+    console.log("App started...");
 
     // Call corresponding render functions based on title of current page using switch statement
     switch (document.title) {
@@ -91,10 +71,7 @@
         displayContact();
         break;
     }
-    
-  
   }
 
-  window.addEventListener('load', Start);
-
+  window.addEventListener("load", Start);
 })();
