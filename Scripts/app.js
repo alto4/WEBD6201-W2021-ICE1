5efa806e7f0b1cf5d73d902bb099f80fc9fd7274
Scott Alton <scottaltonmusic@gmail.com>
2021-01-21 08:41:33 -0500

    Practiced injecting DOM elements on homepage.

--- Scripts/app.js
+++ Scripts/app.js
@@ -11,25 +11,44 @@
   {
     console.log('App started...');
 
-    let heroContainer = document.querySelector('.hero');
     let paragraphOneText = 'This is a simple site to demonstrate DOM Manipulation for ICE 1';
     let paragraphOneElement = document.getElementById('paragraphOne');
  
     paragraphOneElement.textContent = paragraphOneText;
     paragraphOneElement.className = 'fs-5 text-success';
-    heroContainer.className = 'text-center my-5';
-
+  
     // Create new element in memory
     let newParagraph = document.createElement('p');
     // Configure element
     newParagraph.setAttribute('id', 'paragraphTwo');
     newParagraph.textContent = '...paragraph two injected into DOM!';
     newParagraph.className = 'fs-5 text-danger'
+    // Select the parent element
+    let heroContainer = document.querySelector('.hero');
+    heroContainer.className = 'my-5';
     // Inject element into the DOM
     heroContainer.appendChild(newParagraph);
 
-    let paragraphThree = `<p class="fs-7 fw-bold">And finally, here is the paragraph three.</p>`;
-    heroContainer.innerHTML += paragraphThree;
+    // Alternate way to inject element 
+    let paragraphDiv = document.createElement('div');
+    let paragraphThree = `<p id="paragraphThree" class="fs-7 fw-bold">This is the third paragraph, set in the middle using .before().</p>`;
+    paragraphDiv.innerHTML = paragraphThree;
+
+    // Insert new paragraph -> .before inserts right before node
+    // newParagraph.before(paragraphDiv);
+    newParagraph.after(paragraphDiv);
+   
+    // Deletion
+    // paragraphOneElement.remove();
+    heroContainer.removeChild(paragraphOneElement);
+    
+    // Update
+    //heroContainer.firstElementChild.textContent = 'Hello there friends on the internet.';
+  
+    heroContainer.innerHTML = 
+    `<h1 id="firstHeading">Welcome to WEBD6201 - Lab 1</h1>
+    <p id="paragraphOne" class="fs-3 fw-bold text-success">This is my first paragraph</p>
+    `;
   }
 
   window.addEventListener('load', Start);
